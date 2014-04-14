61850bdaf713bf163e65885ef7f133a292ea4eea
jake <jake@coffshire.com>
2014-04-14 09:41:02 -0500

    better algo, the goroutine version is slightly slower

--- DNA/dna.go
+++ DNA/dna.go
@@ -1,47 +1,53 @@
 package main
 
-import "strings"
-
 func CountNucleotides(dna string) (A int, C int, G int, T int) {
-	A = strings.Count(dna, "A")
-	C = strings.Count(dna, "C")
-	G = strings.Count(dna, "G")
-	T = strings.Count(dna, "T")
+	for i := 0; i < len(dna); i++ {
+		switch {
+		case dna[i] == 'A':
+			A++
+		case dna[i] == 'C':
+			C++
+		case dna[i] == 'G':
+			G++
+		case dna[i] == 'T':
+			T++
+		}
+	}
 	return
 }
 
 type nucCount struct {
-	Nuc   string
-	Count int
+	A int
+	C int
+	G int
+	T int
 }
 
 func CountNucleotidesParallel(dna string) (A int, C int, G int, T int) {
 	c := make(chan nucCount)
+	l := len(dna)
 	go func() {
-		c <- nucCount{"A", strings.Count(dna, "A")}
+		A, C, G, T := CountNucleotides(dna[0 : l/4])
+		c <- nucCount{A, C, G, T}
 	}()
 	go func() {
-		c <- nucCount{"C", strings.Count(dna, "C")}
+		A, C, G, T := CountNucleotides(dna[l/4 : l/2])
+		c <- nucCount{A, C, G, T}
 	}()
 	go func() {
-		c <- nucCount{"G", strings.Count(dna, "G")}
+		A, C, G, T := CountNucleotides(dna[l/2 : l*3/4])
+		c <- nucCount{A, C, G, T}
 	}()
 	go func() {
-		c <- nucCount{"T", strings.Count(dna, "T")}
+		A, C, G, T := CountNucleotides(dna[l*3/4:])
+		c <- nucCount{A, C, G, T}
 	}()
 	for i := 0; i < 4; i++ {
 		msg := <-c
-		switch {
-		case msg.Nuc == "A":
-			A = msg.Count
-		case msg.Nuc == "C":
-			C = msg.Count
-		case msg.Nuc == "G":
-			G = msg.Count
-		case msg.Nuc == "T":
-			T = msg.Count
-		}
-
+		A += msg.A
+		C += msg.C
+		G += msg.G
+		T += msg.T
 	}
 	return
 }
